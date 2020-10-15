d235f5b084e476e4e00e0b987903b409d429a4c0
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-10-15 16:11:24 +0900

    05

---
+++ main-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void sub(void);
+
+
+int main(int argc, char *argv[]) {
+
+int i;
+for(i=0;i<3;i++)
+   sub();
+   
+	return 0;
+}
+
+void sub(void)
+{ 
+int auto_count=0;
+auto int static_count =0;
+auto_count++;
+static_count++;
+
+printf("auto_count=%d\n", auto_count);
+printf("static_count=%d\n", static_count);
+}
