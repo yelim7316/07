2c7f9d1d9034aa1c890dd010c6f4fbd0283074fe
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-10-15 16:23:04 +0900

    06

--- main-1.c
+++ main-1.c
@@ -3,25 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
+int all_files;
 
+extern void sub();
 
 int main(int argc, char *argv[]) {
+	
+	sub();
+	printf("%d\n", all_files);
 
-int i;
-for(i=0;i<3;i++)
-   sub();
-   
 	return 0;
 }
 
-void sub(void)
-{ 
-int auto_count=0;
-auto int static_count =0;
-auto_count++;
-static_count++;
 
-printf("auto_count=%d\n", auto_count);
-printf("static_count=%d\n", static_count);
-}
